7bff744d67ada1113b6872484dd006a8326c2d95
Urszula Molska <urszula_molska@wp.pl>
2023-03-07 21:00:14 +0100

    update

--- contacts.js
+++ contacts.js
@@ -1,18 +1,13 @@
-//import fs
 const fs = require("fs").promises;
 
-//import path
 const path = require("path");
-const contacts = require("./db/contacts.json");
 const contactsPath = path.join(__dirname, "db", "contacts.json");
 
-//Function ListContacts
 async function listContacts() {
   const listOfContacts = await GetContacts();
   console.table(listOfContacts);
 }
 
-//async function GetContacts
 async function GetContacts() {
   const data = await fs.readFile(contactsPath);
   const dataToString = data.toString();
@@ -20,7 +15,6 @@ async function GetContacts() {
   return parsedData;
 }
 
-//Function checkNumberOfContacts
 async function checkNumberOfContacts() {
   const data = await fs.readFile(contactsPath);
   const dataToString = data.toString();
@@ -29,38 +23,39 @@ async function checkNumberOfContacts() {
   return numberOfContacts;
 }
 
-//Function getContactsById
 async function getContactById(contactId) {
   const contactID = String(contactId);
   const data = await fs.readFile(contactsPath);
   const parsedData = JSON.parse(data);
+  console.log();
 
-  const idOfContact = parsedData.find((element) => element.id === contactID);
-  console.log(idOfContact);
-  if (parsedData.every((data) => data.id !== contactID) === true) {
+  if (parsedData.every((data) => data.id !== contactID)) {
+    console.log(typeof data.id);
+    console.log(typeof contactId);
     console.log(`there is no id ${contactID} in database please try again!`);
+  } else {
+    const idOfContact = parsedData.find((element) => element.id === contactID);
+    console.log(idOfContact);
   }
 }
 
-//Function removeContactyId
 async function removeContact(contactId) {
-  const actualParsedContactsJsonFile = await GetContacts();
+  const contacts = await GetContacts();
   const contactID = String(contactId);
-  const index = actualParsedContactsJsonFile.findIndex(
+  const isContactFound = contacts.findIndex(
     (element) => element.id === contactID
   );
 
-  const removedContacts = actualParsedContactsJsonFile.splice(index, 1);
+  const removedContacts = contacts.splice(isContactFound, 1);
   console.log("removedContacts", removedContacts);
-  const dataToAdd = JSON.stringify(actualParsedContactsJsonFile);
+  const dataToAdd = JSON.stringify(contacts);
   const dataToAddString = `${dataToAdd}`;
   await fs.writeFile(contactsPath, dataToAddString).then(listContacts);
 }
 
-//Function addContact
 async function addContact(name, email, phone) {
   const lastId = await checkNumberOfContacts();
-  const actualParsedContactsJsonFile = await GetContacts()();
+  const contacts = await GetContacts();
   const nextId = String(lastId + 1);
 
   const contact = {
@@ -70,9 +65,9 @@ async function addContact(name, email, phone) {
     phone: String(phone),
   };
 
-  actualParsedContactsJsonFile.push(contact);
+  contacts.push(contact);
 
-  const dataToAdd = JSON.stringify(actualParsedContactsJsonFile);
+  const dataToAdd = JSON.stringify(contacts);
   const dataToAddString = `${dataToAdd}`;
 
   await fs.writeFile(contactsPath, dataToAddString).then(listContacts);
--- index.js
+++ index.js
@@ -8,22 +8,18 @@ const {
 
 function invokeAction({ action, id, name, email, phone }) {
   switch (action) {
-    //node index.js --action list
     case "list":
       listContacts();
       break;
 
-    //node index.js --action get --id 5
     case "get":
       getContactById(id);
       break;
 
-    //node index.js --action add --name Mango --email mango@gmail.com --phone 322-22-22
     case "add":
       addContact(name, email, phone);
       break;
 
-    //node index.js --action remove --id 3
     case "remove":
       removeContact(id);
       break;
